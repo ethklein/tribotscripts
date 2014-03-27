b461e7c2ae09dde152113649a95296e199adc2ea
JewishBotter <ekleindev@gmail.com>
2014-03-26 22:39:42 -0400

    TFLAirRunecrafter

---
+++ TFLAirRunecrafter/TFLAirRunecrafter.java
@@ -0,0 +1,30 @@
+package scripts.TFLAirRunecrafter;
+
+import java.awt.Graphics;
+
+import org.tribot.script.EnumScript;
+import org.tribot.script.ScriptManifest;
+import org.tribot.script.interfaces.Painting;
+
+@ScriptManifest(authors = { "" }, category = "", name = "")
+public class TFLAirRunecrafter extends EnumScript<TFLAirRunecrafterENUM> implements Painting{
+
+	@Override
+	public void onPaint(Graphics arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public TFLAirRunecrafterENUM getInitialState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TFLAirRunecrafterENUM handleState(TFLAirRunecrafterENUM arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
